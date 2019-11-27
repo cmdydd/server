d2ac486f37afc46113c791b5258c131c896199d4
cmd <m18737169911_2@163.com>
2019-11-26 17:44:24 -0800

    const

--- const.py
+++ const.py
@@ -15,7 +15,9 @@ img_tree_format = "{}/picture/{}_tree.png"
 img_impr_format = "{}/picture/{}_importance.png"
 result_file_format = "{}/data/result/{}_ret.csv"
 err_format = "err&{}"
-ceshi1_path = "data/smote/fif_ceshi/ceshi1.csv"
+ceshi1_path  = "data/smote/fif_ceshi/ceshi1.csv"
+
 ceshi2_path = "data/smote/fif_ceshi/ceshi2.csv"
 ceshi3_path = "data/smote/fif_ceshi/ceshi3.csv"
+
 
